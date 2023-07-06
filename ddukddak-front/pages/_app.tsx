e20904ab4fcc4be0c147944e74a351ba08f550d6
oio337a <yongmipa@student.42seoul.kr>
2023-07-06 21:17:22 +0900

    feat: working on the main page

--- ddukddak-front/pages/_app.tsx
+++ ddukddak-front/pages/_app.tsx
@@ -3,7 +3,7 @@ import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="w-full max-w-xl mx-auto">
+    <div>
       <Component {...pageProps} />
     </div>
   );
