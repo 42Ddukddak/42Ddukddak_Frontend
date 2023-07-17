fb17c65c2afa6820cc2cce74ddcc62dac9992501
oio337a <yongmipa@student.42seoul.kr>
2023-07-17 21:39:44 +0900

    [feat]: login logic

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,22 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import React, { useEffect } from 'react';
 import axios from 'axios';
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const route = useRouter();
   useEffect(() => {
     const url = new URL(window.location.href);
 
     const postData = async () => {
       const code = url.searchParams.get('code');
       try {
-        const response = await axios.post('api/auth/42login', { data: code });
-        console.log(response.data);
+        await axios
+          .post('/api/auth/42login', null, {
+            params: { code: code },
+          })
+          .then((res) => console.log(res));
+        route.push('/');
       } catch (error) {
         console.error(error);
       }
