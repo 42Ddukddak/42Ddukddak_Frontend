4cd99ae958c1199e0b4113437ed5aad5c77373a9
oio337a <yongmipa@student.42seoul.kr>
2023-07-22 16:38:17 +0900

    [feat]: user input message validate check

--- libs/inputMessage.ts
+++ libs/inputMessage.ts
@@ -1,9 +1,17 @@
 import { ChangeEvent, useState } from 'react';
+import messageValidate from './messageValidate';
 
 const useHandleInputMessage = () => {
   const [inputMessage, setInputMessage] = useState('');
   const handleInputMessage = (e: ChangeEvent<HTMLInputElement>) => {
-    setInputMessage(e.target.value);
+    const temp = e.target.value.trim().replace(' ', '');
+    const checkValidate = messageValidate(temp);
+    console.log(checkValidate);
+    if (checkValidate.includes('*')) {
+      alert('욕설 사용하면 되요~?? 안되요~??');
+    } else {
+      setInputMessage(e.target.value);
+    }
   };
   const handleDeleteInputMessage = () => {
     setInputMessage('');
--- pages/enter.tsx
+++ pages/enter.tsx
@@ -2,30 +2,64 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import tempPic from '../public/Ddukddak_logo.png';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loading from '@/components/loading';
 
 const Enter: NextPage = () => {
   const route = useRouter();
+  const [loading, setLoading] = useState(false);
   const onClick = () => {
     route.push('/api/42login');
   };
 
+  useEffect(() => {
+    const url = new URL(window.location.href);
+
+    const postData = async () => {
+      const code = url.searchParams.get('code');
+      try {
+        await axios
+          .post('/api/auth/42login', null, {
+            params: { code: code },
+          })
+          .then(() => setLoading(false))
+          .then(() => route.push('/'));
+        // route.push('/');
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (url.pathname === '/auth/callback') {
+      postData();
+      setLoading(true);
+    }
+  }, []);
+
   return (
     <div className="mt-16 px-16 flex flex-col justify-center items-center space-y-5">
-      <Image src={tempPic} alt="mainIMG" placeholder="blur" />
-      <div>
-        <h2 className="text-2xl font-semibold text-gray-800 border-b-2 border-dashed border-purple-300 my-4 pb-3 text-center">
-          뚝딱에 오신 여러분을 환영합니다.
-        </h2>
-        <span className="text-sm text-gray-500">
-          '뚝딱' 은 익명을 보장합니다. 이야기를 나누거나 모임을 만들어 보세요.
-        </span>
-      </div>
-      <button
-        onClick={onClick}
-        className=" rounded-xl shadow-xl bg-purple-500 text-white px-10 py-4 text-2xl hover:ring-2 hover:ring-offset-2 hover:ring-purple-400 hover:bg-purple-400 focus:outline-none"
-      >
-        Sign in
-      </button>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div>
+          <Image src={tempPic} alt="mainIMG" placeholder="blur" />
+          <div>
+            <h2 className="text-2xl font-semibold text-gray-800 border-b-2 border-dashed border-purple-300 my-4 pb-3 text-center">
+              뚝딱에 오신 여러분을 환영합니다.
+            </h2>
+            <span className="text-sm text-gray-500">
+              '뚝딱' 은 익명을 보장합니다. 이야기를 나누거나 모임을 만들어 보세요.
+            </span>
+          </div>
+          <button
+            onClick={onClick}
+            className=" rounded-xl shadow-xl bg-purple-500 text-white px-10 py-4 text-2xl hover:ring-2 hover:ring-offset-2 hover:ring-purple-400 hover:bg-purple-400 focus:outline-none"
+          >
+            Sign in
+          </button>
+        </div>
+      )}
     </div>
   );
 };
