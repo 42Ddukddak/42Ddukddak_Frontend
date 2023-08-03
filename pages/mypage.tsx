3c1bfd59414438858c45f502b426130605214372
oio337a <yongmipa@student.42seoul.kr>
2023-08-03 22:01:01 +0900

    [feat]: mypage start

--- pages/mypage.tsx
+++ pages/mypage.tsx
@@ -1,22 +1,13 @@
 import JoinedDdukddak from '@/components/joinedDdukddak';
 import Layout from '@/components/layout';
 import PrivateChatting from '@/components/privateChatting';
-import { IResponse } from '@/interface/Context';
-import { createContext, useState } from 'react';
 
-type IContents = [IResponse | null, React.Dispatch<React.SetStateAction<IResponse>>];
-
-export const AppContents = createContext<IContents>([null, () => null]);
 export default function Mypage() {
-  const [contents, setContexts] = useState<IContents>([null, () => null]);
-
   return (
     <Layout>
       <div className="grid gap-4 py-20 px-8 xl:grid-cols-3 h-screen">
-        <AppContents.Provider value={[contents, setContexts]}>
-          <JoinedDdukddak />
-          {contents ? <PrivateChatting mypage /> : null}
-        </AppContents.Provider>
+        <JoinedDdukddak />
+        <PrivateChatting mypage />
       </div>
     </Layout>
   );
