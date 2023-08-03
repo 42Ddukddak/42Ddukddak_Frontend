dc13b8d0feb4940e32454c76089f99c1c00a5c6a
oio337a <yongmipa@student.42seoul.kr>
2023-08-03 17:12:31 +0900

    [fix]: already position same room

--- components/wholeDdukddak.tsx
+++ components/wholeDdukddak.tsx
@@ -47,6 +47,7 @@ export default function WholeDdukddak() {
     if (target?.roomId) {
       if (info.roomInfo?.roomId === target?.roomId) {
         alert('당신이 있는 방이야 이놈아.');
+        setTarget(null);
       } else {
         if (info.roomInfo?.roomId) {
           setText({
@@ -62,7 +63,6 @@ export default function WholeDdukddak() {
         setIsOpen(true);
       }
     }
-    setTarget(null);
   }, [target?.roomId]);
 
   // 방 클릭 이벤트 리스너
