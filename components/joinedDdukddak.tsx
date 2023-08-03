aa669448ff6b03ddff845cb151bac2296c1e6019
oio337a <yongmipa@student.42seoul.kr>
2023-08-03 22:12:11 +0900

    [feat]: request room list get

--- components/joinedDdukddak.tsx
+++ components/joinedDdukddak.tsx
@@ -2,22 +2,28 @@ import axios from 'axios';
 import RightBlockHeader from './rightBlockHeader';
 import { MouseEvent, useEffect, useState } from 'react';
 import { IResponse } from '@/interface/Context';
+import { cls } from '@/libs/utils';
 
 export default function JoinedDdukddak() {
   const [roomList, setRoomList] = useState<Array<IResponse>>([]);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
 
-  // useEffect(() => {
-  //   const requestRoomList = async () => {
-  //     await axios.get('/api/chat/joined').then((res) => {
-  //       setRoomList(res.data);
-  //     });
-  //   };
-  //   requestRoomList();
-  // }, []);
+  useEffect(() => {
+    const requestRoomList = async () => {
+      try {
+        await axios.get('/api/chat/joined').then((res) => {
+          setRoomList(res.data);
+          console.log('@@@@@@@ requestRoomList', res.data);
+        });
+      } catch (error) {
+        console.log('@@@@@@@ requestRoomList err', error);
+      }
+    };
+    requestRoomList();
+  }, []);
 
-  const onClick = (event: MouseEvent<HTMLDivElement>) => {
-    console.log(event.currentTarget.getAttribute('data-custom'));
-    console.log(event);
+  const onClick = (i: number) => {
+    setSelectedIndex(i);
   };
 
   return (
@@ -27,9 +33,11 @@ export default function JoinedDdukddak() {
         {roomList.map((items, i) => (
           <div
             key={i}
-            data-custom={JSON.stringify(roomList[i])}
-            onClick={onClick}
-            className="flex justify-between px-8 py-3 hover:shadow-sm hover:bg-slate-50 cursor-pointer"
+            onClick={() => onClick(i)}
+            className={cls(
+              selectedIndex === i ? ' bg-violet-400' : '',
+              'flex justify-between px-8 py-3 hover:shadow-sm hover:bg-slate-50 cursor-pointer',
+            )}
           >
             <h2 className="self-center text-lg font-semibold w-3/5">{items.roomName}</h2>
             <div className="flex flex-col justify-center items-end space-y-1">
