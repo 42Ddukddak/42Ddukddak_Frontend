6c57c73cc1ca7add1ce31547f10de3e60b2f9524
oio337a <yongmipa@student.42seoul.kr>
2023-08-03 21:11:55 +0900

    [fix]: num please

--- components/privateChatting.tsx
+++ components/privateChatting.tsx
@@ -31,7 +31,7 @@ export default function PrivateChatting({ mypage }: IMypageProps) {
   const messageEndRef = useRef<HTMLDivElement | null>(null);
   const [info, setInfo] = useContext(AppContext);
   const intraId = getCookieValue('intraId');
-  const [changeValues, setChangeValues] = useState<IChangeValues>({ participantsNum: 0 });
+  const [changeValues, setChangeValues] = useState<IChangeValues>({ participantsNum: 1 });
   const [roomIsGone, setRoomIsGone] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [isConfirm, setIsConfirm] = useContext(ModalContext);
@@ -60,7 +60,6 @@ export default function PrivateChatting({ mypage }: IMypageProps) {
     try {
       await axios.get(`/api/chat/private/${info.roomInfo?.roomId}`).then((response) => {
         setChatMessageList(response.data);
-        console.log(response.data);
       });
     } catch (err) {
       console.log('private chatting get', err);
@@ -171,8 +170,8 @@ export default function PrivateChatting({ mypage }: IMypageProps) {
 
   // '뚝딱뚝딱' button 클릭 이벤트 예약 확정
   const onReservation = () => {
-    // 1번은 0 아니면 2(채팅 쳤을 때) 2번은 1
-    if (chatMessageList[0].participantsNum > 1) {
+    // 1번은 1 아니면 2(채팅 쳤을 때) 2번은 1
+    if (chatMessageList[-1].participantsNum > 1) {
       setType('reservation');
       setText({
         title: ModalMessage.RESERVATION.title,
@@ -249,19 +248,23 @@ export default function PrivateChatting({ mypage }: IMypageProps) {
   };
 
   // 전달 받은 메세지 뿌려줄 박스
-  const msgBox = chatMessageList.map((item, idx) => (
-    <div key={idx}>
-      <div className={cls(item.sender === intraId ? 'items-end' : '', 'flex flex-col justify-end pr-4')}>
-        <div className={cls(item.sender === intraId ? '' : 'flex-row-reverse', 'flex justify-end items-end')}>
-          <span className="text-sm text-gray-600 font-light px-2">{formatTime(item.time)} </span>
-          <div className="px-2 py-2  border border-gray-300 rounded-xl bg-violet-300 text-white">
-            <p>{item.message}</p>
+  const msgBox = chatMessageList.map((item, idx) => {
+    item.message ? (
+      <div key={idx}>
+        <div className={cls(item.sender === intraId ? 'items-end' : '', 'flex flex-col justify-end pr-4')}>
+          <div className={cls(item.sender === intraId ? '' : 'flex-row-reverse', 'flex justify-end items-end')}>
+            <span className="text-sm text-gray-600 font-light px-2">{formatTime(item.time)} </span>
+            <div className="px-2 py-2  border border-gray-300 rounded-xl bg-violet-300 text-white">
+              <p>{item.message}</p>
+            </div>
           </div>
+          {item.sender === intraId ? null : <span className="text-xs text-gray-600 mr-1">신고</span>}
         </div>
-        {item.sender === intraId ? null : <span className="text-xs text-gray-600 mr-1">신고</span>}
       </div>
-    </div>
-  ));
+    ) : (
+      <></>
+    );
+  });
 
   return (
     <div className="xl:col-span-2 flex flex-col justify-between border-2 rounded-3xl shadow-xl px-5 py-4 space-y-2 h-screen max-h-[50vh] xl:min-h-[85vh] bg-indigo-300">
