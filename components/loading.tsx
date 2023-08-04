63bd64703252dca2a6f75af5be05af9e04d9b60c
oio337a <yongmipa@student.42seoul.kr>
2023-08-04 14:37:58 +0900

    [style]: loading page styling

--- components/loading.tsx
+++ components/loading.tsx
@@ -6,9 +6,9 @@ export default function Loading() {
     <div className="flex flex-col justify-center items-center h-screen">
       <Image src={tempPic} alt="mainIMG" placeholder="blur" className="w-auto h-auto pb-[40px]" />
       <div></div>
-      <div className="text-[300px] animate-bounce">👀</div>
-      <div className="animate-spin text-5xl font-bold text-gray-900">로딩중...💻</div>
-      <div className="underline text-gray-600 mt-2">현재 로그인 API를 요청하고 있는 것 같네요...</div>
+      <div className="text-[200px] animate-bounce">👀</div>
+      <div className="text-5xl font-bold text-gray-900">로딩중...💻</div>
+      <div className="underline text-gray-600 mt-2">현재 API를 요청하고 있는 것 같네요...</div>
     </div>
   );
 }
