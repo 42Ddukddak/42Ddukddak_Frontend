a48f9c476eac55616c646579cda5fa765f9129e4
oio337a <oio337a@gmail.com>
2023-08-07 22:03:35 +0900

    [fix]: modal closed delete

--- components/modal.tsx
+++ components/modal.tsx
@@ -17,20 +17,14 @@ export default function Modal({ title, subText, setIsOpen, setTime }: ModalProps
     setReservedTime(e.target.value);
   };
 
-  const onClick = () => {
-    setIsOpen(false);
-  };
   return (
     <div className="flex justify-center items-center h-screen fixed z-[2]">
-      <div
-        onClick={onClick}
-        className="bg-black h-screen bg-opacity-40 w-screen flex justify-center items-center fixed top-0 left-0"
-      >
+      <div className="bg-black h-screen bg-opacity-40 w-screen flex justify-center items-center fixed top-0 left-0">
         <div className="bg-white bg-opacity-100 flex flex-col justify-between items-center border rounded-3xl w-[480px] h-96 shadow-xl">
           <h2 className="text-3xl mt-12 text-violet-700">{title}</h2>
           <span className="text-xl font-semibold">{subText}</span>
           {setTime ? (
-            <div className='flex justify-center items-center space-x-4'>
+            <div className="flex justify-center items-center space-x-4">
               <span className="text-xl font-semibold">시간</span>
               <input onChange={handleTimeValue} type="time" className="border-2 border-gray-300 rounded-lg w-full" />
             </div>
