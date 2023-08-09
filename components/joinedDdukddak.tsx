33c5a278eb33e5f2ea864315b9dec91dc07d4cf7
oio337a <yongmipa@student.42seoul.kr>
2023-08-09 19:06:58 +0900

    [chore]: function sep

--- components/joinedDdukddak.tsx
+++ components/joinedDdukddak.tsx
@@ -18,18 +18,19 @@ export default function JoinedDdukddak({ setIndex }: IIndexProps) {
   const [selectedIndex, setSelectedIndex] = useState<number>();
   const [mouseOnIndex, setMouseOnIndex] = useState<number | undefined>();
 
+  const requestRoomList = async () => {
+    try {
+      await axios.get('/api/reserved/list').then((res) => {
+        setRoomList(res.data);
+      });
+    } catch (error) {
+      console.log('@@@@@@@ requestRoomList err', error);
+    }
+  };
+
   useEffect(() => {
-    const requestRoomList = async () => {
-      try {
-        await axios.get('/api/reserved/list').then((res) => {
-          setRoomList(res.data);
-        });
-      } catch (error) {
-        console.log('@@@@@@@ requestRoomList err', error);
-      }
-    };
     requestRoomList();
-  }, []);
+  });
 
   const onClick = (i: number) => {
     setSelectedIndex(i);
@@ -42,6 +43,12 @@ export default function JoinedDdukddak({ setIndex }: IIndexProps) {
 
   const handleMouseOut = (i: number) => {
     setMouseOnIndex(i);
+  };
+
+  const onCancel = (i: number) => {
+    // 예약 취소 api 요청
+    // OK status 오면 requestRoomList 호출
+    console.log(i);
   };
 
   const roomBox = roomList.map((items) => (
@@ -57,14 +64,20 @@ export default function JoinedDdukddak({ setIndex }: IIndexProps) {
       <h2 className="self-center text-lg font-semibold w-3/5">{items.roomName}</h2>
       <div className="flex justify-center items-end">
         {mouseOnIndex === items.roomId ? (
-          <span className="bg-slate-400 py-1 px-2 rounded-md shadow-md">취소</span>
-        ) : null}
-        <span className=" bg-violet-500 py-1 px-2 rounded-md text-white shadow-md">예약 시간 {items.reservedTime}</span>
+          <span
+            onClick={() => onCancel(items.roomId)}
+            className="bg-slate-400 py-1 px-2 rounded-md shadow-md shadow-violet-300"
+          >
+            취소
+          </span>
+        ) : (
+          <span className=" bg-violet-500 py-1 px-2 rounded-md text-white shadow-md">
+            예약 시간 {items.reservedTime}
+          </span>
+        )}
       </div>
     </div>
   ));
-
-  // ---- test ---- ///
 
   return (
     <div className="flex flex-col border-2 rounded-3xl py-4 px-5 shadow-2xl h-screen max-h-[50vh] xl:min-h-[85vh]">
@@ -72,27 +85,6 @@ export default function JoinedDdukddak({ setIndex }: IIndexProps) {
       <div onMouseOut={() => handleMouseOut(-1)} className="divide-y-2 mt-2 overflow-auto">
         {roomBox}
       </div>
-      {[1].map((items) => (
-        <div
-          key={items.roomId}
-          onClick={() => onClick(items.roomId)}
-          onMouseOver={() => handleMouseOver(items.roomId)}
-          className={cls(
-            selectedIndex === items.roomId ? ' bg-violet-400' : '',
-            'flex justify-between px-8 py-3 shadow-sm hover:bg-violet-400 cursor-pointer rounded-lg',
-          )}
-        >
-          <h2 className="self-center text-lg font-semibold w-3/5">{items.roomName}</h2>
-          <div className="flex justify-center items-end">
-            {mouseOnIndex === items.roomId ? (
-              <div className="bg-slate-400 py-1 px-2 rounded-md shadow-md shadow-violet-500">취소</div>
-            ) : null}
-            <span className=" bg-violet-500 py-1 px-2 rounded-md text-white shadow-md">
-              예약 시간 {items.reservedTime}
-            </span>
-          </div>
-        </div>
-      ))}
     </div>
   );
 }
